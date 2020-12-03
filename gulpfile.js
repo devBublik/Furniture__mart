4f49c063e23a87205606ce008e8da4853aa7a06d
devBublik <ew.sorokina2011@yandex.ru>
2020-12-03 23:41:45 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -47,8 +47,7 @@ gulp.task('style', function () {
 	return gulp.src([
 			'node_modules/normalize.css/normalize.css',
 			'node_modules/slick-carousel/slick/slick.css',
-			'node_modules/magnific-popup/dist/magnific-popup.css',
-			'node_modules/rateyo/src/jquery.rateyo.css'
+			'node_modules/magnific-popup/dist/magnific-popup.css'
 		])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
