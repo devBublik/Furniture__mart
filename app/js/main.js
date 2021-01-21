ed3c73a3ec201280a189855c2f081dc14a449237
devbublik <ew.sorokina2011@ya.ru>
2021-01-21 14:14:23 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,14 @@
 $(function() {
 
-
+    new WOW().init();
 
     $('.rateYo').rateYo({});
 
-    $('.products__inner').mixItUp();
+    // $('.products__inner').mixItUp();
+
+
+    // $('#countdown').countdown({
+    //     until: liftoffTime
+    // });
 
 });
